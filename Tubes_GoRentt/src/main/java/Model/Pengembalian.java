6e1fff948cd0a8497a7077dac9920dc90cb6fff2
hanifaditia <hanifaditia.sofian@gmail.com>
2023-11-28 01:31:16 +0700

    onprogress pengembalian class

--- Tubes_GoRentt/src/main/java/Model/Pengembalian.java
+++ Tubes_GoRentt/src/main/java/Model/Pengembalian.java
@@ -4,10 +4,52 @@
  */
 package Model;
 
+import java.util.Date;
+
 /**
  *
  * @author Holanta
  */
 public class Pengembalian {
+    private Pembayaran pembayaran;
+    private Date tanggal_kembali;
+    private int denda;
+    private boolean status_pengembalian;
+
+    public Pembayaran getPembayaran() {
+        return pembayaran;
+    }
+
+    public void setPembayaran(Pembayaran pembayaran) {
+        this.pembayaran = pembayaran;
+    }
+
+    public Date getTanggal_kembali() {
+        return tanggal_kembali;
+    }
+
+    public void setTanggal_kembali(Date tanggal_kembali) {
+        this.tanggal_kembali = tanggal_kembali;
+    }
+
+    public int getDenda() {
+        return denda;
+    }
+
+    public void setDenda(int denda) {
+        this.denda = denda;
+    }
+
+    public boolean isStatus_pengembalian() {
+        return status_pengembalian;
+    }
+
+    public void setStatus_pengembalian(boolean status_pengembalian) {
+        this.status_pengembalian = status_pengembalian;
+    }
     
+    public void createPembayaran(int jumlah_bayar){
+        this.pembayaran = new Pembayaran();
+        
+    }
 }
